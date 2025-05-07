ad007b3a1a37a61e35341a05a31af30839f9a0e1
Coffeebe <159054217+Coffeebe@users.noreply.github.com>
2025-05-07 13:11:23 -0600

    Update milk.ino

--- milk/milk.ino
+++ milk/milk.ino
@@ -1,19 +1,31 @@
+const int motorPin2 = 9;
 const int motorPin = 10;
+const int pin_thing2 = 7;
+const int pin_thing = 8 ;
 int trackerPin = A5;
 int lineVal = 0;
 void setup() {
   Serial.begin(9600);
+  pinMode(pin_thing,OUTPUT);
+  pinMode(pin_thing2,OUTPUT);
   pinMode(motorPin, OUTPUT);
+  pinMode(motorPin2,OUTPUT);
 
+digitalWrite(pin_thing,HIGH);
+digitalWrite(pin_thing2,HIGH);
 }
 
 void loop() {
   lineVal = analogRead(trackerPin);
   Serial.print(lineVal);
   if (lineVal > 500) {
+
 analogWrite(motorPin,230);
+
+analogWrite(motorPin2,230);
   } else{
     analogWrite(motorPin,0);
+    analogWrite(motorPin2,0);
   }
   delay(1000);
 }
